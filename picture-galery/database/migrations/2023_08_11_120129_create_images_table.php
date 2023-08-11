4ccb4ebc468c589e91589d2c2d904a97c0d7a6a9
DESKTOP-RDDTCN0\Norsan-PC <luis.noriega@elaniin.com>
2023-08-11 08:05:54 -0400

    feature/fixImagesTable

--- picture-galery/database/migrations/2023_08_11_120129_create_images_table.php
+++ picture-galery/database/migrations/2023_08_11_120129_create_images_table.php
@@ -6,20 +6,21 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
+      /**
      * LNORIEGA (08/08/2023): Create table 'images' to save the info related to the load images.
      */
     public function up(): void
     {
         Schema::create('images', function (Blueprint $table) {
-        $table->uuid('id')->default(\Illuminate\Support\Str::uuid());
-        $table->string('title');
-        $table->string('details');
-        $table->string('path');
-        $table->string('disks');
-        $table->string('collection_id');
-        $table->timestamp('create_time')->nullable();
-    });
+            $table->uuid('id')->default(\Illuminate\Support\Str::uuid());
+            $table->string('title');
+            $table->string('details');
+            $table->string('path');
+            $table->string('disks');
+            $table->uuid('collection_id')->nullable();
+            $table->timestamp('create_time')->nullable();
+        });
+
     }
 
     /**
