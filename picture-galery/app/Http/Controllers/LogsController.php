d2ce68af59df61890bab34d571ff62f3be72d629
DESKTOP-RDDTCN0\Norsan-PC <luis.noriega@elaniin.com>
2023-08-12 00:09:54 -0400

    feature/deleteTablesExtra

--- picture-galery/app/Http/Controllers/LogsController.php
+++ picture-galery/app/Http/Controllers/LogsController.php
@@ -8,22 +8,6 @@ use Illuminate\Http\Request;
 class LogsController extends Controller
 {
     /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-
-    }
-
-    /**
      * Store a newly created resource in storage.
      */
     public function store(Request $request)
@@ -31,37 +15,5 @@ class LogsController extends Controller
         $logData = $request->all();
         $log = logs::create($logData);
         return response()->json($log, 201);
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(logs $logs)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(logs $logs)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, logs $logs)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(logs $logs)
-    {
-        //
     }
 }
