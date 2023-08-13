fd6c8bc8323539ac75c4a8a11e78d97e2375f92c
DESKTOP-RDDTCN0\Norsan-PC <luis.noriega@elaniin.com>
2023-08-12 21:56:40 -0400

    feature/botonAdicionarEliminarImagenes

--- picture-galery/resources/views/home.blade.php
+++ picture-galery/resources/views/home.blade.php
@@ -85,13 +85,132 @@
         </div>
         <div>
             <div class="col text-center">
-                <button class="btn button_style_glass mt-3">{{ __('Editar galería') }}</button>
+                <button class="btn button_style_glass mt-3" data-bs-toggle="modal" data-bs-target="#myModal">{{ __('Editar galería') }}</button>
                 <p class="custom-font-size mb-0">{{ __('Autor: Luis Noriega') }}</p>
 
             </div>
         </div>
     </div>
-    <script src="script/lightbox-plus-jquery.js"></script>
+     <!-- Modal -->
+     <div class="modal fade" id="myModal" tabindex="-1" aria-labelledby="myModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="myModalLabel"></h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <h2 class="header">Edición de galeria</h2>
+                    <div class="modal-body">
+                        <form>
+                          <div class="mb-3">
+                            <label for="recipient-name" class="col-form-label">Titulo de galeria:</label>
+                            <input type="text" class="form-control" id="recipient-name">
+                          </div>
+                          <div class="mb-3">
+                            <label for="message-text" class="col-form-label">Detalle de galeria:</label>
+                            <textarea class="form-control" id="message-text"></textarea>
+                          </div>
+                        </form>
+                      </div>
+                      <hr>
+                      <p class="d-flex justify-content-between align-items-center">
+                        <span>Añadir nueva imagen a la galeria</span>
+                        <button  id="openFileUpload" class="btn button_style_glass" data-bs-toggle="popover" title="Título del Popover" >
+                            Añadir
+                        </button>
+                    </p>
+                      <hr>
+                      <p class="d-flex justify-content-between align-items-center">
+                        <span>Editar o eliminar imágenes de la galería</span>
+                        <button class="btn button_style_glass" data-bs-toggle="popover" title="Título del Popover" >
+                            Editar
+                        </button>
+                    </p>
+                    <hr>
+                    <p class="d-flex justify-content-between align-items-center">
+                        <span>Eliminar la galería actual</span>
+                        <button class="btn button_style_glass" data-bs-toggle="modal" data-bs-dismiss="modal" data-bs-target="#modalSecundario" >
+                            Eliminar
+                        </button>
+                    </p>
+                    <hr>
+                    <p class="d-flex justify-content-center align-items-center">
+                        <button class="btn button_style_glass" data-bs-toggle="popover" title="Título del Popover">
+                            Guardar cambios
+                        </button>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Modal secundario -->
+<div class="modal fade" id="modalSecundario" tabindex="-1" aria-labelledby="modalSecundarioLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="header" id="modalSecundarioLabel">Atención!</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <p>Esta seguro que desea eliminar esta colección? al hacer click en el boton "Eliminar" se eliminará la colección permanentemente asi como tambien las fotografias contenidas en dicha colección.</p>
+                <p class="d-flex justify-content-center align-items-center" style="color: brown">
+                <button class="btn button_style_glass"  >
+                    Eliminar
+                </button>
+            </p>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+    <input type="file" id="fileInput" accept=".jpg, .jpeg, .png, .tif" style="display:none;">
+    <script src="script/lightbox-plus-jquery.js">
+     $(document).ready(function () {
+        $('[data-bs-toggle="popover"]').popover();
+        $('[data-bs-toggle="tooltip"]').tooltip();
+    });
+    </script>
+    {{-- <script>
+        document.addEventListener("DOMContentLoaded", function() {
+                var modalButton = document.getElementById("modalButton");
+
+                modalButton.addEventListener("click", function() {
+                    var confirmResult = window.confirm("¿Estás seguro de eliminar la galeria?");
+
+                    if (confirmResult) {
+                        alert("Eliminación confirmada");
+                    } else {
+                        alert("Eliminación cancelada");
+                    }
+                });
+            });
+    </script> --}}
+    <script>
+        var openFileUpload = document.getElementById("openFileUpload");
+        var fileInput = document.getElementById("fileInput");
+
+        openFileUpload.addEventListener("click", function() {
+            fileInput.click();
+        });
+
+        fileInput.addEventListener("change", function() {
+            var selectedFile = fileInput.files[0];
+            if (selectedFile) {
+                var validFormats = [".jpg", ".jpeg", ".png", ".tif"];
+                var fileExtension = selectedFile.name.substring(selectedFile.name.lastIndexOf(".")).toLowerCase();
+
+                if (validFormats.includes(fileExtension)) {
+                    alert("Imagen válida seleccionada: " + selectedFile.name);
+                } else {
+                    alert("Formato de archivo no válido. Selecciona una imagen en formato jpg, jpeg, png o tif.");
+                    fileInput.value = "";
+                }
+            }
+        });
+    </script>
     <footer>
         <div class="container">
             <hr><div class="col text-center">
