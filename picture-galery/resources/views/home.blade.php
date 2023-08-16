deaf8bf1816107f2e5196d7a6260eab9fc1bf547
DESKTOP-RDDTCN0\Norsan-PC <luis.noriega@elaniin.com>
2023-08-16 12:30:24 -0400

    feature/fixCarouselSelect

--- picture-galery/resources/views/home.blade.php
+++ picture-galery/resources/views/home.blade.php
@@ -71,32 +71,86 @@
     </div>
     <div class="container mt-5">
         <div id="carouselExample2" class="carousel slide">
-            <div class="carousel-inner">
-                {{-- @php
-                   dd($imagesByCollection[0]['users_id']) ;
-                @endphp --}}
-                @php
-                    $numCards = count($imagesByCollection);
-                @endphp
-                @for ($i = 0; $i < $numCards; $i += 2)
-                <div class="carousel-item @if($i === 0)active @endif">
-                    <div class="d-flex justify-content-center">
-                        @php
-                            $end = min($i + 2, $numCards);
-                        @endphp
-                        <div >
-                            <a href="{{ route('collection.show', [
-                                'collection_id' => 'f6e5ccbe-3085-463d-af1e-442e0ada244e',
-                                'collection_title' => 'Crea tu nueva Galería!',
-                                'users_id' => $imagesByCollection[0]['users_id'],
-                                'collection_details' => 'Presiona el boton \'\'Editar\'\' para adicionar imagenes y editar tu nueva galería'
-                            ]) }}">
-                            <img src="{{ asset('../storage/public/images/new_button.png') }}" style="width:19rem" alt="">
-                        </a>
+            <div id="carouselExample" class="carousel slide" data-bs-ride="carousel">
+                <div class="carousel-inner">
+                    @php
+                        $numCards = count($imagesByCollection);
+                    @endphp
+                    @if ($numCards > 0)
+                    @for ($i = 0; $i < $numCards; $i += 4)
+                    <div class="carousel-item @if($i === 0)active @endif" data-bs-interval="8000">
+                        <div class="d-flex justify-content-center">
+                            @php
+                                $end = min($i + 4, $numCards);
+                            @endphp
+                           @for ($j = $i; $j < $end; $j++)
+                           @if($imagesByCollection[$j]['users_id'] === '5')
+                            @if ($j===0)
+                            <div class="card mx-2" style="height: 400px;">
+                                <div class="img-hover-zoom">
+                                    <div >
+                                        <a href="{{ route('collection.show', [
+                                            'collection_id' => 'f6e5ccbe-3085-463d-af1e-442e0ada244e',
+                                            'collection_title' => 'Crea tu nueva Galería!',
+                                            'users_id' => $imagesByCollection[0]['users_id'],
+                                            'collection_details' => 'Presiona el boton \'\'Editar\'\' para adicionar imagenes y editar tu nueva galería'
+                                        ]) }}">
+                                        <img src="{{ asset('../storage/public/images/new_button.png') }}" style="width:19rem" alt="">
+                                    </a>
+                                    </div>
+                                </div>
+                            </div>
+                           @endif
+                           <div class="card mx-2" style="height: 400px;">
+                               <div class="img-hover-zoom">
+                                   <a href="{{ route('collection.show', [
+                                       'collection_id' => $imagesByCollection[$j]['collection_id'],
+                                       'collection_title' => $imagesByCollection[$j]['collection_title'],
+                                       'users_id' => $imagesByCollection[$j]['users_id'],
+                                       'collection_details' => $imagesByCollection[$j]['collection_details']
+                                   ]) }}">
+                                       <img src="{{ $imagesByCollection[$j]['path'] }}" class="card-img-top img-fluid" alt="{{ $imagesByCollection[$j]['title'] }}" style="max-width: 100%; max-height: 100%;">
+                                       <div class="card-body">
+                                           <h3 class="carousel-title">{{ $imagesByCollection[$j]['collection_title'] }}</h3>
+                                       </div>
+                                   </a>
+                               </div>
+                           </div>
+                        @endif
+                    @endfor
                         </div>
                     </div>
+                    @endfor
+                    @else
+                    <div class="carousel-item @if($i === 0)active @endif" data-bs-interval="8000">
+                        <div class="d-flex justify-content-center">
+                            <div class="d-flex justify-content-center">
+                                @php
+                                    $end = min($i + 2, $numCards);
+                                @endphp
+                                <div >
+                                    <a href="{{ route('collection.show', [
+                                        'collection_id' => 'f6e5ccbe-3085-463d-af1e-442e0ada244e',
+                                        'collection_title' => 'Crea tu nueva Galería!',
+                                        'users_id' => $imagesByCollection[0]['users_id'],
+                                        'collection_details' => 'Presiona el boton \'\'Editar\'\' para adicionar imagenes y editar tu nueva galería'
+                                    ]) }}">
+                                    <img src="{{ asset('../storage/public/images/new_button.png') }}" style="width:19rem" alt="">
+                                </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    @endif
                 </div>
-                @endfor
+                <a class="carousel-control-prev" href="#carouselExample" role="button" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </a>
+                <a class="carousel-control-next" href="#carouselExample" role="button" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </a>
             </div>
             <a class="carousel-control-prev" href="#carouselExample2" role="button" data-bs-slide="prev">
                 <span class="carousel-control-prev-icon" aria-hidden="true"></span>
